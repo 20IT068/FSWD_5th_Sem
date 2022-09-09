51a9cc957f09939dddc6ee122ca45fc388c9bc92
20IT068 <111736553+20IT068@users.noreply.github.com>
2022-09-09 22:51:03 +0530

    Added NavigationBar

--- studentmanagementsystemfrontend-Copy/src/App.js
+++ studentmanagementsystemfrontend-Copy/src/App.js
@@ -1,31 +1,14 @@
-import "./App.css";
-import Student from "./components/Student";
-import StudentList from "./components/StudentList";
-import NavigationBar from "./components/NavigationBar";
-import Footer from "./components/Footer";
-import Container from "react-bootstrap/Container";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MyAlert from "./components/MyAlert";
-import { useState } from "react";
+import NavigationBar from './components/NavigationBar';
+import Student from './components/Student';
+import StudentList from './components/StudentList';
 
 function App() {
-  const [alert, setAlert] = useState(null);
-
-  let showAlert = (type, message) => {
-    setAlert({
-      type: type,
-      message: message,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  };
-
   return (
     <div className="App">
-      
+      <NavigationBar/>
+      <Student/>
+      <StudentList/>
     </div>
   );
 }
-
 export default App;
